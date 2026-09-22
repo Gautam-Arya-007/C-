81d4a1d67d27efc6c162fae31d0cd126044558fd
Gautam-Arya-007 <gautamarya9256@gmail.com>
2026-09-22 23:18:09 +0530

    Adding two files

---
+++ simpleinterest.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float p = 10,r = 2,t = 2;
+    float si = p*r*t/100;
+    cout<<"The simple interest is "<<si;
+    cin.get();
+}
---
+++ volumeofsphere.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    float r = 5;
+    float volume = 3.141592*r*r*r*4/3;
+    cout<<"The volume of sphere is "<<volume;
+    cin.get();
+}
